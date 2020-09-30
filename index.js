1794bc8abd8e85ccd78e2ac760be756a5ab159ac
Jener Sigua <siguajener@yahoo.com>
2020-09-30 15:09:20 +0800

    add index.html

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-var express = require('express');
-var app = express(); 
-// serves files from the root directory
-app.use(express.static('./'));
-app.listen(8080, function () {    
-  console.log('Listening at http://localhost:8080');  
+const http = require('http');
+const fs = require('fs');
+const port = 3000;
+
+const server = http.createServer(function(req, res){
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    fs.readFile('index.html', function(error, data) {
+        if (error) {
+            res.writeHead(404);
+            res.write('Error: file not found');
+        } else {
+            res.write(data);
+        }
+        res.end();
+    });
 });
+
+server.listen(port, function(error) {
+    if (error) {
+        console.log('Something went wrong', error);
+    } else {
+        console.log('Server is listening on port ' + port)
+    }
+})
